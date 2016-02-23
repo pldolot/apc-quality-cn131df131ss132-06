c4cec42464fd684a598cc3452b8d78b62188c862
mprivera <mprivera1101@gmail.com>
2016-02-24 02:32:47 +0800

    New modules user

--- application/advanced/backend/config/main.php
+++ application/advanced/backend/config/main.php
@@ -11,7 +11,11 @@ return [
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
-    'modules' => [],
+    'modules' => [
+        'user' => [
+            'class' => 'backend\modules\user\User',
+        ],
+    ],
     'components' => [
         'user' => [
             'identityClass' => 'common\models\User',
