570fc43fdd1bd914dd0737ccbc7fe506411aeb2a
Patrick Vonn Dolot <Patrick Vonn Dolot>
2016-04-05 23:46:37 +0800

    Edited the design of employee

--- application/advanced/backend/views/employee/_form.php
+++ application/advanced/backend/views/employee/_form.php
@@ -15,26 +15,67 @@ use yii\helpers\ArrayHelper;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'id_number')->textInput(['maxlength' => true]) ?>
+<style type="text/css">
+    .column1{
+        width:400px;
+        height:300px;
+        position:absolute;
+        text-align:left;
+    }
+        .column2{
+            width:400px;
+            height:300px;
+            position:absolute;
+            padding-left: 10px;
+            text-align:left;
+            margin-left:400px;
+            
 
-    <?= $form->field($model, 'firstname')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'lastname')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'middlename')->textInput(['maxlength' => true]) ?>
-
-    
+            
+    }
+        .column3{
+                width:400px;
+                height:300px;
+                position:absolute;
+                padding-left: 10px;
+                text-align:left;
+                margin-left:825px;
+                margin-top:75px;
+                
+    }
+            .column4{
+            width:100px;
+            height:70px;
+            position:absolute;
+            margin-left:525px;
+            margin-top:200px;
+            text-align:left;
+        }
+</style>
+<div class="column1">
+    <?= $form->field($model, 'id_number')->textInput(['maxlength' => true, 'style' => 'width: 300px']) ?>
+    <?= $form->field($model, 'lastname')->textInput(['maxlength' => true, 'style' => 'width: 300px']) ?>
+    </div>
+<div class="column2">
     <?php
 
 $positions=Position::find()->all();
 
 $listData=ArrayHelper::map($positions,'position_id','position_name');
 
-echo $form->field($model, 'position_id')->dropDownList($listData,['prompt'=>'Select Position']);
+echo $form->field($model, 'position_id')->dropDownList($listData,['prompt'=>'Select Position', 'style' => 'width: 300px']);
 
 ?>
+    <?= $form->field($model, 'firstname')->textInput(['maxlength' => true, 'style' => 'width: 300px']) ?>
+    </div>
+<div class="column3">
+    <?= $form->field($model, 'middlename')->textInput(['maxlength' => true, 'style' => 'width: 300px']) ?>
+    </div>
+<div class="column4">
+   
+    </div>
 
-    <div class="form-group">
+    <div class="column4">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
