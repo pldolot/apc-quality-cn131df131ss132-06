e71458ec771f9830b14b8656e845f965459e687c
mprivera <mprivera1101@gmail.com>
2016-04-05 21:21:00 +0800

    adding RBAC in ERD design(Edited Version)

--- application/advanced/backend/views/site/index.php
+++ application/advanced/backend/views/site/index.php
@@ -7,15 +7,12 @@ $this->title = 'My Yii Application';
 <div class="site-index" style="margin-left:0px">
 
   <ul class="nav nav-tabs" >
-  <li role="presentation" class="active"><a href="#">Home</a></li>
+  <li role="presentation" class="active"><a href="http://localhost/softdev/backend/web/index.php?r=scc-case">Home</a></li>
 
   <li role="presentation"><a href="http://localhost/softdev/backend/web/index.php?r=scc-case">Case</a></li>
   <li role="presentation"><a href="http://localhost/softdev/backend/web/index.php?r=employee">Employee</a></li>
   <li role="presentation"><a href="http://localhost/softdev/backend/web/index.php?r=position">Positions</a></li>
   <li role="presentation"><a href="http://localhost/softdev/backend/web/index.php?r=profile">BEI/BOC Profile</a></li>
-
-
-
 
 </ul>
 
