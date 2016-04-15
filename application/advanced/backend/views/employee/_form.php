e0c51a8840c3bfabd3e06ede790b7e95b6aefae8
Patrick Vonn Dolot <Patrick Vonn Dolot>
2016-04-15 20:49:10 +0800

    Edited BEI/BOC Profile form and employee form

--- application/advanced/backend/views/employee/_form.php
+++ application/advanced/backend/views/employee/_form.php
@@ -2,33 +2,87 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use common\models\Position;
+use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Employee */
 /* @var $form yii\widgets\ActiveForm */
 ?>
+<style type="text/css">
+    
+    .column1{
+        width:350px;
+        height:500px;
+        position: absolute;
+        margin-bottom: 50px;
+        padding-left: 10px;
+        
+    }
+
+    .column2{
+        width:350px;
+        height:400px;
+        position: absolute;
+        padding-left:10px;
+        margin-left:420px;
+        margin-bottom:50px; 
+        
+    }
+    .column3{
+        width:350px;
+        height:400px;
+        position: absolute;
+        padding-left:10px;
+        margin-left:530px;
+        margin-top:325px; 
+
+    }
+
+</style>    
 
 <div class="employee-form">
 
     <?php $form = ActiveForm::begin(); ?>
+<div class= 'column1'>
+    <?= $form->field($model, 'lastname')->textInput(['maxlength' => true, 'style' => 'width: 300px']) ?>
+    <?= $form->field($model, 'firstname')->textInput(['maxlength' => true, 'style' => 'width: 300px']) ?>
+    <?= $form->field($model, 'middlename')->textInput(['maxlength' => true, 'style' => 'width: 300px']) ?>
+     <?= $form->field($model, 'sex')->dropDownList([ 'Male' => 'Male', 'Female' => 'Female', ], ['prompt' => 'Select your sex', 'style' => 'width: 300px']) ?>
+</div>
+<div class= 'column2'>
+    
+     <?= $form->field($model, 'user_id')->textInput(['maxlength' => true, 'style' => 'width: 300px']) ?>
+    <?php
+        $position=Position::find()->all();
 
-    <?= $form->field($model, 'id_number')->textInput(['maxlength' => true]) ?>
+        $listData=ArrayHelper::map($position,'position_id','position_name');
+        echo $form->field($model, 'position_id')->dropDownList($listData,['prompt'=>'Select your position', 'style' => 'width: 300px']);
 
-    <?= $form->field($model, 'firstname')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'lastname')->textInput(['maxlength' => true]) ?>
+    ?>
+    <?= $form->field($model, 'id_number')->textInput(['maxlength' => true, 'style' => 'width: 300px']) ?>
+     <?php
+        $position=Position::find()->all();
 
-    <?= $form->field($model, 'middlename')->textInput(['maxlength' => true]) ?>
+        $listData=ArrayHelper::map($position,'position_id','position_name');
+        echo $form->field($model, 'position_id')->dropDownList($listData,['prompt'=>'Select your position', 'style' => 'width: 300px']);
 
-    <?= $form->field($model, 'position_id')->textInput() ?>
 
-    <?= $form->field($model, 'user_id')->textInput() ?>
+    ?>
+</div>
+<div class= 'column3'>
+               <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    
+</div>
+    
 
-    <?= $form->field($model, 'sex')->dropDownList([ 'Male' => 'Male', 'Female' => 'Female', ], ['prompt' => 'Select your sex']) ?>
 
-    <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-    </div>
+    
+    
+
+    
+
 
     <?php ActiveForm::end(); ?>
 
